b5819d5ac737cda0196e5a3b9d936803ddd1b4eb
Tlg <tuligen@navinfo.com>
2020-04-06 20:07:54 +0800

    compile

--- build.sh
+++ build.sh
@@ -1,5 +1,26 @@
+#build thirdparty lib
+cd Thirdparty 
+#DBoW2
+cd DBoW2
 mkdir build
 cd build
 cmake ..
-make -j
-../output/Positioning
+make -j4
+#g2o
+cd ../../g2o
+mkdir build && cd build
+cmake ..
+make -j4
+#geographiclib
+cd ../../GeographicLib
+mkdir build && cd build
+cmake ..
+make -j4
+
+#build positioning
+#cd ../../../
+#mkdir build
+#cd build
+#cmake ..
+#make -j
+#../output/Positioning
